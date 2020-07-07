26acf46676853e02d46fbe496e7fe90e08d06de1
Ankita Nema <nema.ankita26@gmail.com>
2020-07-07 13:58:22 +0530

    Adding other options login button

--- starter-kit/mobile-app/src/app/components/login/login.component.ts
+++ starter-kit/mobile-app/src/app/components/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {APIService} from '../../services/api-service'; 
-import { Router} from '@angular/router';
+import { APIService } from '../../services/api-service';
+import { Router } from '@angular/router';
 import { DataService } from '../../services/data-service';
-import { ListPicker } from '@nativescript/core/ui/list-picker';
+import { ListPicker } from '@nativescript/core';
 import { LogInService } from '../../services/login-service';
 import { LoaderService } from '../../services/loader-service';
+import { openUrl } from '@nativescript/core/utils/utils';
 
 @Component({
   selector: 'app-login',
@@ -17,75 +18,79 @@ export class LoginComponent implements OnInit {
   password: string;
   userType: string;
   userTypeList: string[];
-  userDetails = {}; 
+  userDetails = {};
   error: boolean;
   errorText: string;
 
-  constructor(public loaderService: LoaderService,private apiService: APIService, private router: Router, private dataService: DataService, private loginService: LogInService) { }
+  constructor(public loaderService: LoaderService, private apiService: APIService, private router: Router, private dataService: DataService, private loginService: LogInService) { }
 
   ngOnInit() {
     this.loginService.userDetail = {};
     this.loginService.isLoggedIn = false;
     this.userTypeList = this.dataService.userType;
-  }  
+  }
 
-  routeToSignUp(){
+  routeToHhWebsite() {
+    openUrl('https://helpinghand1.mybluemix.net/api/newlogin');
+  }
+
+  routeToSignUp() {
     this.router.navigate(['/signup']);
   }
 
-  routeToServiceProvider(){
+  routeToServiceProvider() {
     this.router.navigate(['/serviceProvider']);
   }
 
-  routeToServiceProviderOrder(){
+  routeToServiceProviderOrder() {
     this.router.navigate(['/order']);
   }
 
-  routeTocontributor(){
+  routeTocontributor() {
     this.router.navigate(['/contributor']);
   }
 
   login() {
     this.errorText = "";
     this.error = false;
-    if(this.userDetails['username'] && this.userDetails['password']){
+    if (this.userDetails['username'] && this.userDetails['password']) {
       this.apiService.login(this.userDetails)
-      .subscribe(data=>{
-        console.log("Login --------------->>>>>>>>>>");
-        console.log(data);
-        if(data.error){
-          console.log(data.error);
-          this.errorText = data.error;
-          this.error = true;
-        }else{
-          this.loginService.userDetail = data;
-          this.loginService.isLoggedIn = true;
+        .subscribe(data => {
+          console.log("Login --------------->>>>>>>>>>");
           console.log(data);
-          this.error = false;
-          switch(data.role) {
-            case 'SERVICE_PROVIDER':
-              this.routeToServiceProviderOrder();
-              break;
-            case 'CONSUMER':
-              this.routeToServiceProvider();
-              break;
-            case 'CONTRIBUTOR':
-              this.routeTocontributor();
-              break; 
-            default:
+          if (data.error) {
+            console.log(data.error);
+            this.errorText = data.error;
+            this.error = true;
+          } else {
+            this.loginService.userDetail = data;
+            this.loginService.isLoggedIn = true;
+            console.log(data);
+            this.error = false;
+            switch (data.role) {
+              case 'SERVICE_PROVIDER':
+                this.routeToServiceProviderOrder();
+                break;
+              case 'CONSUMER':
+                this.routeToServiceProvider();
+                break;
+              case 'CONTRIBUTOR':
+                this.routeTocontributor();
+                break;
+              default:
               // code block
-          }    
-        }   
-      })
-    }else{
+            }
+          }
+        })
+    } else {
       this.errorText = "Please Enter All Details";
       this.error = true;
     }
-    
+
   }
 
-public userTypeChanged(args) {
+  public userTypeChanged(args) {
     const picker = <ListPicker>args.object;
     this.userDetails['role'] = this.userTypeList[picker.selectedIndex];
-}
+  }
 }
